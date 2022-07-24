5fb64bf2527baa4bd6827cfd4292f1d52a72e0c5
muhammad ali syaifudin <muhammad.ali.syaifudin@hotmail.com>
2022-07-24 22:36:42 +0800

    navbar pt1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import IconButton from './Atom/IconButton';
+import { MdInfo } from 'react-icons/md';
+import { BsFillGearFill } from 'react-icons/bs';
+import { IoStatsChartSharp } from 'react-icons/io5';
 
 function Navbar() {
   return (
-    <div className='sticky top-0 bg-blue-400'>
-      <button>info</button>
-      <h1>title</h1>
-      <button>stats</button>
-      <button>settings</button>
+    <div className="sticky top-0 dark:bg-zinc-800 bg-blue-500 p-2 text-white">
+      <div className="max-w-3xl mx-auto flex justify-between items-center">
+        <IconButton>
+          <MdInfo fontSize={26} />
+        </IconButton>
+        <div className="flex text-sm items-end">
+          <h1 className="font-extrabold text-4xl">RASLI</h1>
+          <p>v0.3.0</p>
+        </div>
+        <div className="flex">
+          <IconButton>
+            <IoStatsChartSharp fontSize={26} />
+          </IconButton>
+          <IconButton>
+            <BsFillGearFill fontSize={26} />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
