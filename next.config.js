83b21f7366790dd658c010b564aae458fe63d57f
Ali <muhammad.ali.syaifudin@hotmail.com>
2022-08-17 18:18:08 +0700

    add material ui

--- next.config.js
+++ next.config.js
@@ -4,17 +4,15 @@ const { env } = require('./src/server/env');
 const nextTranslate = require('next-translate')
 
 /** @type {import('next').NextConfig} */
-const nextConfig =  nextTranslate({
-  webpack: (config, { isServer, webpack }) => {
-    return config;
-  },
+const nextConfig =  {
 	reactStrictMode: true,
+  swcMinify: true,
   publicRuntimeConfig: {
     NODE_ENV: env.NODE_ENV,
-  },
-})
+  }
+}
 
 /**
  * @link https://nextjs.org/docs/api-reference/next.config.js/introduction
  */
-module.exports = nextConfig
+module.exports = nextTranslate(nextConfig)
--- src/components/ThemeModeBtn.tsx
+++ src/components/ThemeModeBtn.tsx
@@ -1,8 +1,9 @@
-import React, {  useId } from "react";
+import React, {  useContext, useId } from "react";
 import { BsFillSunFill } from "react-icons/bs";
 import { IoIosMoon } from "react-icons/io";
 import { useTheme } from "next-themes";
 import Toggle from "./Atom/Toggle";
+import { ColorModeContext } from "~/pages/_app";
 
 export interface ThemeModeBtnProps {
 	mode: "light" | "dark";
@@ -10,10 +11,13 @@ export interface ThemeModeBtnProps {
 
 export default function ThemeModeBtn() {
 	const id = useId();
+	const colorMode = useContext(ColorModeContext);
 	const { theme, setTheme } = useTheme();
 	// true means dark mode
 	const handleClick = (state: boolean) => {
-		setTheme(state ? "dark" : "light");
+		const newTheme = state ? "dark" : "light"
+		setTheme(newTheme);
+		colorMode.toggleColorMode(newTheme);
 	};
 
 	return (
